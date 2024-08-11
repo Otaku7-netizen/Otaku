83a4c6a04c47f5438a47d65af5bb7912fa175e42
Otaku7-netizen <okasharajpoot30@gmail.com>
2024-08-11 05:16:05 +0500

    started on home and reuseable components

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+
+        <footer className="bg-gray-800 p-4 text-white text-center">
+          <p>&copy; 2024 My Next.js App</p>
+        </footer>
+    );
+}
+
+export default Footer
